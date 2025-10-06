66507073f13ccec8c9d5a409d976d59a497cfb74
Kevin Thaller <kevin909thaller@gmail.com>
2025-10-06 19:20:49 +0200

    Add IRepository

---
+++ Lunchbag.API/Services/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq.Expressions;
+
+namespace Lunchbag.API.Services
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<T?> GetByIdAsync(int id);
+        Task<IEnumerable<T>> GetAllAsync();
+        Task AddAsync(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        Task<bool> ExistsAsync(int id);
+        Task<bool> SaveChangesAsync();
+    }
+}
