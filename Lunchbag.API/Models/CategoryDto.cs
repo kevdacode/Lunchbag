8bbba594b484d81db35cec0562c383f7b1c562a7
Kevin Thaller <kevin909thaller@gmail.com>
2025-10-06 18:53:56 +0200

    Add Mapping Profile & Dto for Category

---
+++ Lunchbag.API/Models/CategoryDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Lunchbag.API.Models
+{
+    public class CategoryDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+    }
+}
---
+++ Lunchbag.API/Profiles/CategoryProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+
+namespace Lunchbag.API.Profiles
+{
+    public class CategoryProfile : Profile
+    {
+        public CategoryProfile() {
+            CreateMap<Entities.Category, Models.CategoryDto>();
+            CreateMap<Models.CategoryDto, Entities.Category>();
+        }
+    }
+}
