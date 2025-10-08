cb5109f12a8f84057bbf1142e362c8acaa8b5e28
Kevin Thaller <kevin909thaller@gmail.com>
2025-10-08 16:54:19 +0200

    Add Mapping Profile & Dto for Product

---
+++ Lunchbag.API/Models/ProductDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Lunchbag.API.Models
+{
+    public class ProductDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+        public CategoryDto Category { get; set; }
+    }
+}
---
+++ Lunchbag.API/Profiles/ProductProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+
+namespace Lunchbag.API.Profiles
+{
+    public class ProductProfile : Profile
+    {
+        public ProductProfile()
+        {
+
+            CreateMap<Entities.Product, Models.ProductDto>();
+            CreateMap<Models.ProductForCreationDto, Entities.Product>();
+        }
+    }
+}
