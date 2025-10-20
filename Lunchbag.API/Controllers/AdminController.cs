6abf3e0c9b25a126df1603b7f819995de1c0216d
Kevin Thaller <kevin909thaller@gmail.com>
2025-10-20 18:51:00 +0200

    Refactor OrdersController to AdminController

--- Lunchbag.API/Controllers/AdminController.cs
+++ Lunchbag.API/Controllers/AdminController.cs
@@ -1,36 +1,33 @@
 ﻿using AutoMapper;
-using Lunchbag.API.Entities;
 using Lunchbag.API.Models;
 using Lunchbag.API.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using System.Threading.Tasks;
 
 namespace Lunchbag.API.Controllers
 {
-    [Route("api/orders")]
+    [Authorize(Policy = "AdminOnly")]
+    [Route("api/admin")]
     [ApiController]
-    public class OrdersController : ControllerBase
+    public class AdminController : ControllerBase
     {
         private readonly IOrderRepository _orderRepository;
-        private readonly IProductRepository _productRepository;
         private readonly IMapper _mapper;
 
-        public OrdersController(IOrderRepository orderRepository, IProductRepository productRepository, IMapper mapper)
+        public AdminController(IOrderRepository orderRepository, IMapper mapper)
         {
             _orderRepository = orderRepository ?? throw new ArgumentNullException(nameof(orderRepository));
-            _productRepository = productRepository ?? throw new ArgumentNullException(nameof(productRepository));
             _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
-
         }
 
-        [HttpGet]
+        [HttpGet("orders")]
         public async Task<ActionResult<IEnumerable<OrderDto>>> GetAllOrders()
         {
             var orders = await _orderRepository.GetAllAsync();
             return Ok(_mapper.Map<IEnumerable<OrderDto>>(orders));
         }
 
-        [HttpGet("{id}", Name = "GetOrder")]
+        [HttpGet("order/{id}", Name = "GetOrder")]
         public async Task<ActionResult<OrderDto>> GetOrder(int id)
         {
             var order = await _orderRepository.GetByIdAsync(id);
@@ -41,35 +38,7 @@ namespace Lunchbag.API.Controllers
             return Ok(_mapper.Map<OrderDto>(order));
         }
 
-        [HttpPost]
-        public async Task<ActionResult> CreateOrder(OrderForCreationDto order)
-        {
-            var orderToAdd = _mapper.Map<Order>(order);
-            foreach(OrderItem o in orderToAdd.OrderItems)
-            {
-                var product = await _productRepository.GetByIdAsync(o.ProductId);
-
-                if (product == null || product.Stock < o.Quantity) 
-                {
-                    return BadRequest("Insufficient stock.");
-                }
-                o.UnitPrice = product.Price;
-                orderToAdd.TotalAmount += o.Quantity * o.UnitPrice;
-            }
-
-            await _orderRepository.AddAsync(orderToAdd);
-            await _orderRepository.SaveChangesAsync();
-
-            var savedOrder = await _orderRepository.GetByIdAsync(orderToAdd.Id);
-            var orderToReturn = _mapper.Map<OrderDto>(savedOrder);
-            return CreatedAtRoute("GetOrder", new
-            {
-                id = orderToReturn.Id,
-            }, orderToReturn);
-
-        }
-
-        [HttpPatch("{id}/finalize")]
+        [HttpPatch("order/{id}/finalize")]
         public async Task<ActionResult> FinalizeOrder(int id, UpdateOrderFinalizedDto updateOrderFinalizedDto)
         {
             var orderToFinalize = await _orderRepository.GetByIdAsync(id);
@@ -80,8 +49,6 @@ namespace Lunchbag.API.Controllers
             }
 
             orderToFinalize.Finalized = updateOrderFinalizedDto.Finalized;
- 
-            _orderRepository.Update(orderToFinalize);
             await _orderRepository.SaveChangesAsync();
 
             var savedOrder = await _orderRepository.GetByIdAsync(id);
@@ -90,7 +57,7 @@ namespace Lunchbag.API.Controllers
             return Ok(orderToReturn);
         }
 
-        [HttpDelete("{id}")]
+        [HttpDelete("order/{id}")]
         public async Task<ActionResult> DeleteOrder(int id)
         {
             var order = await _orderRepository.GetByIdAsync(id);
